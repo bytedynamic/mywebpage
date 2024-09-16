765f926c44e0dd2c8a3358b52390beb7abcb2eec
lighthouse.tecent.cloud <lighthouse@tecent.cloud>
2024-09-16 20:51:14 +0800

    2024-09-16 20:51:14

--- push2git.sh
+++ push2git.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd /home/lighthouse/gitee/runstatus/codeoncloud
+git pull --progress -v --no-rebase -- "origin"
 git add .
 git commit -m "$(date "+%Y-%m-%d %H:%M:%S") "
 git push -u origin "master"
